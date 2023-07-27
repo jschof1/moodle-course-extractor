8c00f24610e608cf55e8451b031d103d2e7c321a
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-07-27 11:11:46 +0100

    readme, sanatize, doc convert

---
+++ README.md
@@ -0,0 +1,59 @@
+# Moodle Course Extractor
+
+This GitHub repository contains the Moodle Course Extractor application which allows you to extract course content from Moodle. The application provides an easy-to-use method of extracting course content and reformatting it into both an HTML file and a Google Docs file.
+
+Please note, this application does not currently support the extraction of images due to the way Moodle handles file backups.
+
+## Prerequisites
+
+Before running this application, you will need to install the following:
+
+- [Node.js](https://nodejs.org/en/download/)
+- [npm](https://www.npmjs.com/get-npm) (comes bundled with Node.js)
+
+## Installation
+
+After you have installed Node.js and npm, clone the repository to your local machine:
+
+```bash
+git clone https://github.com/YourGithubUsername/moodle-course-extractor.git
+cd moodle-course-extractor
+```
+
+Next, install the project dependencies:
+
+```bash
+npm install
+```
+
+## Usage
+
+To use the Moodle Course Extractor application, follow these steps:
+
+1. **Backup your Moodle Course**: In Moodle, navigate to the course you want to extract and create a backup. Download the backup to your local machine.
+2. **Rename and Unzip the Backup**: Change the file extension of the downloaded backup from `.mbz` to `.zip`. Once the file extension has been changed, unzip the file.
+3. **Locate the Activities Folder**: Inside the unzipped folder, look for the "activities" folder.
+4. **Move the Activities Folder**: Drag the "activities" folder into the Moodle Course Extractor folder.
+5. **Run the Application**: In your terminal, navigate to the Moodle Course Extractor folder and run the following command:
+
+```bash
+node xml-to-html
+```
+
+After running the command, the application will create two files: an HTML file containing the raw course content and a `.docx` file that can be imported directly into Google Drive.
+
+## Limitations
+
+Please note that the current version of this application does not support the extraction of images. This is due to how Moodle handles file backups, which prevents images from being included in the backup.
+
+## Contributing
+
+Contributions to the Moodle Course Extractor are welcomed. To contribute, please submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. For more details, see the [LICENSE](./LICENSE) file.
+
+## Contact
+
+If you have any questions or issues, please submit an issue in this repository. You can also contact the repository owner directly at YourGithubUsername@example.com.
--- xml-to-html.js
+++ xml-to-html.js
@@ -1,78 +1,87 @@
-import path from 'path';
-import fs from 'fs';
-import util from 'util';
-import xml2js from 'xml2js';
-
-// Use util.promisify to convert callback-based functions to Promise-based.
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const xml2js = require("xml2js");
+const htmlToDocx = require("html-to-docx");
+const sanitizeHtml = require("sanitize-html");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const readdir = util.promisify(fs.readdir);
 
-let contents = []; // To store the contents of all files
+async function convertHtmlToDocx(html) {
+  const buffer = await htmlToDocx(html);
+  fs.writeFileSync("output.docx", buffer);
+}
+
+function sanitizeContent(htmlContent) {
+  const allowedTags = sanitizeHtml.defaults.allowedTags.filter(
+    (tag) => tag !== "img"
+  );
+  return sanitizeHtml(htmlContent, { allowedTags });
+}
+
+let contents = [];
 
 async function convertXmlToHtml(xml) {
-    // Convert XML to JSON
-    const parser = new xml2js.Parser();
-    const json = await parser.parseStringPromise(xml);
-    // Extract the content field
-    let content = "";
-    let type = "";
+  const parser = new xml2js.Parser();
+  const json = await parser.parseStringPromise(xml);
 
-    // Handle 'page' activity
-    if (json.activity && json.activity.page && json.activity.page[0].content) {
-        content = json.activity.page[0].content[0];
-        type = "Page Activity";
-    }
+  let content = "";
+  let type = "";
 
-    // Handle 'scorm' activity
-    else if (json.activity && json.activity.scorm && json.activity.scorm[0].intro) {
-        content = json.activity.scorm[0].intro[0];
-        type = "Scorm Activity";
-    }
+  if (json.activity && json.activity.page && json.activity.page[0].content) {
+    content = json.activity.page[0].content[0];
+    type = "Page Activity";
+  } else if (
+    json.activity &&
+    json.activity.scorm &&
+    json.activity.scorm[0].intro
+  ) {
+    content = json.activity.scorm[0].intro[0];
+    type = "Scorm Activity";
+  }
 
-    // Prepend the type to the content
-    if(content){
-        content = `<h2>${type}</h2>\n${content}`;
-    }
+  const html = `${content}`;
 
-    // TODO: Convert content to HTML
-    // Depending on your needs, you might want to wrap `content` in some basic HTML tags
-    const html = `${content}`; 
-
-    return html;
+  return html;
 }
 
 async function processDirectory(dir) {
-    const entries = await readdir(dir, { withFileTypes: true });
+  const entries = await readdir(dir, { withFileTypes: true });
 
-    // Filter out directories that start with 'page_' or 'scorm_'
-    const pageAndScormDirs = entries.filter(entry => entry.isDirectory() && (entry.name.startsWith('page_') || entry.name.startsWith('scorm_')));
+  const pageAndScormDirs = entries.filter(
+    (entry) =>
+      entry.isDirectory() &&
+      (entry.name.startsWith("page_") || entry.name.startsWith("scorm_"))
+  );
 
-    // Extract the trailing number from the directory name, convert it to a number, and sort
-    pageAndScormDirs.sort((a, b) => {
-        const numA = Number(a.name.split('_')[1]);
-        const numB = Number(b.name.split('_')[1]);
-        return numA - numB;
-    });
+  pageAndScormDirs.sort((a, b) => {
+    const numA = Number(a.name.split("_")[1]);
+    const numB = Number(b.name.split("_")[1]);
+    return numA - numB;
+  });
 
-    // Loop over the sorted directories
-    for (const dirent of pageAndScormDirs) {
-        const fullPath = path.join(dir, dirent.name);
-        const subEntries = await readdir(fullPath, { withFileTypes: true });
-        const xmlFiles = subEntries.filter(entry => entry.isFile() && path.extname(entry.name) === '.xml');
+  for (const dirent of pageAndScormDirs) {
+    const fullPath = path.join(dir, dirent.name);
+    const subEntries = await readdir(fullPath, { withFileTypes: true });
+    const xmlFiles = subEntries.filter(
+      (entry) => entry.isFile() && path.extname(entry.name) === ".xml"
+    );
 
-        for (const file of xmlFiles) {
-            const xmlPath = path.join(fullPath, file.name);
-            const xml = await readFile(xmlPath, 'utf-8');
-            const html = await convertXmlToHtml(xml);
-            contents.push(html);
-        }
+    for (const file of xmlFiles) {
+      const xmlPath = path.join(fullPath, file.name);
+      const xml = await readFile(xmlPath, "utf-8");
+      const html = await convertXmlToHtml(xml);
+      contents.push(html);
     }
+  }
 }
-
-processDirectory('moodle-course-extractor/course/activities')
-    .then(() => {
-        // Write all contents to file.html
-        return writeFile('file.html', contents.join('\n'));
-    })
-    .catch(console.error);
+const activitiesPath = path.join(__dirname, "activities");
+processDirectory(activitiesPath)
+  .then(() => {
+    const htmlContent = contents.join('\n');
+    const sanitizedHtmlContent = sanitizeContent(htmlContent);
+    return writeFile('extract.html', sanitizedHtmlContent)
+        .then(() => convertHtmlToDocx(sanitizedHtmlContent));
+  })
+  .catch(console.error);
